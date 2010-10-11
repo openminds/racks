093b6265dd8efc9b84d56dc666514cd97398acbb
Bob Van Landuyt <bob.vanlanduyt@scarlet.be>
2010-10-11 13:08:07 +0200

    Adding new devices is now in dialog

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,8 +2,9 @@
 // This file is automatically included by javascript_include_tag :defaults
 $(function(){
 	//init tabs
-	$("#datacenters").tabs();
-	$("#server_rack_info").tabs();
+	// $("#datacenters").tabs();
+	// $("#server_rack_info").tabs();
+	$('.tabs').tabs();
 	$(".devices_accordion").accordion({collapsible:true, active:false, icons:false, autoHeight:false});
 	//Create buttons
 	$(".button").button();
