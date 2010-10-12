92faee859d6c55c9939c75356d8b5a96b0f3872f
Bob Van Landuyt <bob.vanlanduyt@scarlet.be>
2010-10-12 16:35:17 +0200

    Exctracted the javascript to put in application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -26,7 +26,9 @@ $(function(){
 			return false;
 		}
 	});
-	
+	//create the add interface links
+	$('#add_interface').click(function() {enableInterfaceLinks()});
+	//Create the links to load forms using ajax
 	$("a.remote").live('click', function(){
 		// alert($(this).attr("href"));
 		$("#modal_wrapper").load($(this).attr("href"), "#modal_form", function(response, status, xhr){
@@ -42,11 +44,10 @@ $(function(){
 				width:900
 				});
 			$('#modal_form > form > div.tabs ').tabs();
+			$('#add_interface').click(function() {enableInterfaceLinks()});
 		});
 		return false;
 	});
-	
-	
 });
 //A custom search autocomplete (using categories)
 $.widget( "custom.catcomplete", $.ui.autocomplete, {
@@ -62,6 +63,41 @@ $.widget( "custom.catcomplete", $.ui.autocomplete, {
 		});
 	}
 });
+function enableInterfaceLinks(){
+	//var interface_row = $('.interface_row').fi.html();
+	var $interface_table = $('#interface_form:nth-child(2)');
+	var $interface_row = $interface_table.children(':last-child').children(':last-child').clone();
+	//alert($interface_row.html());
+	$interface_row.find('*[id*=device_interfaces_attributes_]').each(function(index){
+		//set the correct ID
+		var current_id = $(this).attr('id').split('_');
+		current_id[3] = parseInt(current_id[3])+1;
+		var new_id = ''
+		for (var i = current_id.length - 1; i >= 0; i--){
+			new_id =  current_id[i] + new_id 
+			if (i>0) {
+				new_id = '_' + new_id 
+			};
+		};
+		$(this).attr("id", new_id)
+
+		//set the correct name
+		var current_name = $(this).attr('name').split('][');
+		current_name[1] = parseInt(current_name[1])+1;
+		var new_name = ''
+		for (var i = current_name.length - 1; i >= 0; i--){
+			new_name =  current_name[i] + new_name
+			if (i > 0) {
+				new_name = '][' + new_name
+			};
+		};
+		$(this).attr("name", new_name)
+
+		//clear the current values
+		$(this).attr("value", "reset");	
+	});
+	$interface_table.append('<tr class="interface_row">' + $interface_row.html() + '</tr>')
+}
 function getParameterByName( name )
 {
   name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
