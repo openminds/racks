03b0f756c9027e4dedd6992c76b9186eaf392c67
Bob Van Landuyt <bob.vanlanduyt@scarlet.be>
2010-10-13 11:44:01 +0200

    Find companies too

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,8 +3,9 @@ class SearchController < ApplicationController
 		@devices = Device.all(:conditions => ['name LIKE ?', "%#{params[:term]}%"], :order => 'name ASC')
 		@server_racks = ServerRack.all(:conditions => ['name LIKE ?', "%#{params[:term]}%"], :order => 'name ASC')
 		@datacenters = Datacenter.all(:conditions => ['name LIKE ?', "%#{params[:term]}%"], :order => 'name ASC')
+		@companies = Company.all(:conditions => ['name LIKE ?', "%#{params[:term]}%"], :order => 'name ASC')
 		@search = []
-		
+
 		@devices.each do |device|
 			@search << {:label => device.name, :category => 'Devices', :value => device.id, :url => datacenter_server_rack_device_path(device.server_rack.datacenter, device.server_rack, device) }
 		end
@@ -14,11 +15,14 @@ class SearchController < ApplicationController
 		@datacenters.each do |datacenter|
 			@search << {:label => datacenter.name, :category => 'Datacenters', :value => datacenter.id, :url => datacenter_path(datacenter) }
 		end
+		@companies .each do |company|
+			@search << {:label => company.name, :category => 'Companies', :value => company.id, :url => company_path(company) }
+		end
 		respond_to do |format|
 			format.json {render :json => @search }
 		end
 	end
-	
+
 	def find_colors
 		@colors = CableConnection.where("color LIKE :term", :term => "%#{params[:term]}%").collect {|c| c.color}
 		@colors.uniq
@@ -26,7 +30,7 @@ class SearchController < ApplicationController
 			format.json {render :json => @colors }
 		end
 	end
-	
+
 	def search
 		@searchword = params[:s]
 		@found_companies = Company.where("name LIKE :term OR comment LIKE :term", :term => "%#{@searchword}%").order("name ASC")
