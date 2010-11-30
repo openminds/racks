f03d0d32c7718f89ffdd169bee1209c5d17eba79
Bob Van Landuyt <bob.vanlanduyt@scarlet.be>
2010-11-30 12:57:29 +0100

    Forgot to expire the cache for devices the current device will be connected to

--- app/sweepers/device_sweeper.rb
+++ app/sweepers/device_sweeper.rb
@@ -1,20 +1,25 @@
 class DeviceSweeper < ActionController::Caching::Sweeper
-  observe Device
+	observe Device
 
-  def after_save(device)
-    expire_cache(device)
-  end
+	def after_save(device)
+		expire_cache(device)
+	end
 
-  def after_destroy(device)
-    expire_cache(device)
-  end
+	def after_destroy(device)
+		expire_cache(device)
+	end
 
-  private
+	private
 
-    def expire_cache(device)
+	def expire_cache(device)
 		expire_fragment dom_id(current_server_rack)
 		expire_fragment "drawing_#{dom_id(current_server_rack)}"
-      expire_fragment dom_id(device)
-    end
+		expire_fragment dom_id(device)
+		device.interfaces.each do |interface|
+			if interface.other
+				expire_fragment dom_id(interface.other.device)
+			end
+		end
+	end
 
 end
