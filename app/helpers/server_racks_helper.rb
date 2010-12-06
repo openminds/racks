9dc7c12619945fa3c21962452a8f378021f05b23
Bob Van Landuyt <bob.vanlanduyt@scarlet.be>
2010-12-06 11:17:06 +0100

    Extracted rack info into helper

--- app/helpers/server_racks_helper.rb
+++ app/helpers/server_racks_helper.rb
@@ -1,2 +1,12 @@
 module ServerRacksHelper
+	def show_info_for_server_rack(server_rack)
+		comment =  content_tag :p, server_rack.comment
+		concat(comment)
+		available_units = content_tag :p, "Available units: #{server_rack.units.available.size}/#{server_rack.units.size}"
+		concat(available_units)
+		if server_rack.units.available.any?
+			biggest = content_tag :p, "Biggest space: #{server_rack.biggest_available_space.size}U (#{server_rack.biggest_available_space.first.number} - #{server_rack.biggest_available_space.last.number})"
+			concat(biggest)
+		end
+	end
 end
