2113b1c7a744f30b05941a1c269895cbd3dfd6f2
Bob Van Landuyt <bob.vanlanduyt@scarlet.be>
2010-12-17 09:35:47 +0100

    Removed debug code

--- features/step_definitions/javascript_steps.rb
+++ features/step_definitions/javascript_steps.rb
@@ -3,17 +3,10 @@ When /^option "([^"]*)" from "([^"]*)" should be disabled$/ do |label, field|
 	option = select.find(:xpath, XPath::HTML.option(label), :message => "Option not found")
 	raise 'Option was enabled' if option['disabled'] == 'false'
 end
-
 When /^option "([^"]*)" from "([^"]*)" should be enabled$/ do |label, field|
 	select = find(:xpath, XPath::HTML.select(field), :message => "Select list not found")
 	option = select.find(:xpath, XPath::HTML.option(label), :message => "Option not found")
 	raise 'Option was disabled' if option['disabled'] == 'true'
-end
-Then /^debugging_info$/ do
-	server_rack = ServerRack.all.last
-	server_rack.units.each do |unit|
-		puts "Unit: #{unit.number}"
-	end
 end
 When /^I follow "([^"]*)" in the "([^"]*)" fieldset$/ do |link_name, fieldset_title|
 	within_fieldset(fieldset_title) do
